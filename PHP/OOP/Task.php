4591838183763e1b810e2ee708850f17da6f673d
LiBin Shen <3112968354@qq.com>
2017-06-06 14:22:23 +0800

    update

---
+++ PHP/OOP/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+class Task{
+    public $description;
+    public $completed = false;
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+    public function complete()
+    {
+        $this->completed = true;
+    }
+}
+
+$task = new Task('learn oop');
+$task->complete();
+var_dump($task->completed);
