b7d0a3822e4761977667e88a418aa5c316501085
LiBin Shen <3112968354@qq.com>
2017-06-08 12:08:51 +0800

    update

--- Laravel/blog51/database/factories/ModelFactory.php
+++ Laravel/blog51/database/factories/ModelFactory.php
@@ -19,3 +19,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Article::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence,
+        'intro' => $faker->sentence,
+        'content' => $faker->paragraph,
+        'published_at' => \Carbon\Carbon::now(),
+    ];
+});
