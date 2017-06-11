2d196b8a374d74c48d7054070dfe4b28a9a21c34
LiBin Shen <3112968354@qq.com>
2017-06-11 14:22:49 +0800

    update

--- Laravel/laravel51Advanced/app/Console/Kernel.php
+++ Laravel/laravel51Advanced/app/Console/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\Inspire::class,
+        Commands\showGreet::class,
+        Commands\LogInfo::class,
     ];
 
     /**
@@ -26,5 +28,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('inspire')
                  ->hourly();
+        $schedule->command('lesson:log')
+            ->everyMinute();
     }
 }
