aea44b31f204f8c94128713b44fb83c850a428ea
LiBin Shen <3112968354@qq.com>
2017-06-11 18:04:34 +0800

    update

---
+++ Laravel/laravel51Advanced/resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Laravel</title>
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
