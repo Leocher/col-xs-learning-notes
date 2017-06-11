bccf1f65c88345d0b3dda6865f99ff139f31f1de
LiBin Shen <3112968354@qq.com>
2017-06-11 15:01:14 +0800

    update

--- Laravel/laravel51Advanced/app/Http/routes.php
+++ Laravel/laravel51Advanced/app/Http/routes.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::loginUsingId(3);
+Route::group(['prefix'=>'admin','middleware'=>'admin'],function(){
+   Route::get('/users',function(){
+       return 'only admin';
+   }) ;
+});
