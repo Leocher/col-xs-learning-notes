957b72d93d12dbd5cc6bfe9b6fdfecc244b8af1f
LiBin Shen <3112968354@qq.com>
2017-06-14 07:27:15 +0800

    update

---
+++ Laravel/zhihuapp/resources/lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码至少6个字符并且与确认密码字段匹配！',
+    'reset' => '您的密码已经被重置了！',
+    'sent' => '我们已经发送了重置密码链接到您的邮箱！',
+    'token' => '重置密码token无效！',
+    'user' => "没有找到该邮箱对应的信息！",
+
+];
