fca36f0c2ab4fe478b63ce7176637df33d753370
LiBin Shen <3112968354@qq.com>
2017-06-16 23:47:15 +0800

    update

--- Laravel/zhihuapp/resources/views/questions/show.blade.php
+++ Laravel/zhihuapp/resources/views/questions/show.blade.php
@@ -4,18 +4,23 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{ $question->title }}</div>
+                    <div class="panel-heading">
+                        {{ $question->title }}
+                        @foreach($question->topics as $topic)
+                            <a class="topic" href="/topic/{{ $topic->id }}">{{ $topic->name }}</a>
+                        @endforeach
+                    </div>
                     <div class="panel-body">
                         {!! $question->body !!}
                         <!-- 可能产生XSS攻击漏洞，后续会过滤处理 -->
+                    </div>
+                    <div class="actions">
+                        @if(Auth::check() && Auth::user()->owns($question))
+                            <span class="edit"><a href="/questions/{{ $question->id }}/edit">编辑</a></span>
+                        @endif
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <style>
-        .panel-body img{
-            width: 100%;
-        }
-    </style>
 @endsection
