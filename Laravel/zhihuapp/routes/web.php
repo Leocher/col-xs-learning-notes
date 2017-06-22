d5fcd68a773f51fceeabe7be0ad4c89a2c8ca8c8
LiBin Shen <3112968354@qq.com>
2017-06-22 21:17:53 +0800

    私信

--- Laravel/zhihuapp/routes/web.php
+++ Laravel/zhihuapp/routes/web.php
@@ -23,9 +23,10 @@ Route::resource('questions','QuestionsController',['name'=>[
     'create' => 'questions.create',
     'show' => 'questions.show',
 ]]);
-
 Route::post('/questions/{question}/answer','AnswersController@store');
-
 Route::get('question/{question}/follow','QuestionFollowController@follow');
 
 Route::get('notifications','NotificationsController@index');
+
+Route::get('inbox','InboxController@index');
+Route::get('inbox/{userId}','InboxController@show');
