f6b0117e4bed293c199ec5c03b8709392c582ef7
Laraveek <3112968354@qq.com>
2017-06-22 16:57:16 +0800

    Add files via upload

---
+++ sqlsrv.php
@@ -0,0 +1,16 @@
+<?php
+
+'sqlsrv' => [
+    'driver' => 'sqlsrv',
+    'host' => env('DB_HOST', 'tax250'),
+    'database' => env('DB_DATABASE', 'tax9cn'),
+    'username' => env('DB_USERNAME', 'sa'),
+    'password' => env('DB_PASSWORD', 'sa'),
+    'prefix' => '',
+],
+
+DB_CONNECTION=sqlsrv
+DB_HOST=
+DB_DATABASE=
+DB_USERNAME=sa
+DB_PASSWORD=123456
